32822f7df4f4f863fc675db76bb16bd44ca16033
Jack McCown <jmccown@cbinsights.com>
2019-12-09 21:21:30 -0500

    initial POC

--- src/index.js
+++ src/index.js
@@ -1,30 +1,180 @@
-function main(argz) {
-  return Promise.resolve().then(() => {
-    console.log('Hello world!')
-    console.dir(argz)
+const fs = require('fs')
+const { prompt } = require('enquirer')
+const { build: plistBuilder } = require('plist')
+
+async function generatePlist() {
+  const pList = {}
+  const getResult = (...prmpts) =>
+    prompt(prmpts.map((prmpt, idx) => ({ name: `result${idx}`, ...prmpt })))
+      .then(answers =>
+        Object.keys(answers)
+          .filter(k => k.startsWith('result'))
+          .map(rk => answers[rk])
+      )
+      .then(z => {
+        if (z.length > 1) {
+          return z
+        }
+        return z[0]
+      })
+
+  const label = await getResult({
+    type: 'input',
+    message: 'What is the text label for this service?'
   })
-}
+  const cmd = await getResult({
+    type: 'input',
+    message: 'What command should be executed?'
+  })
+  const triggerMethod = await getResult({
+    type: 'select',
+    message: 'How should the command be triggered?',
+    choices: ['Interval Timer', 'Calendar', 'Watch Path']
+  })
+  const triggerConfig = await getResult(
+    {
+      // TODO handle cron like input
+      'Interval Timer': {
+        type: 'input',
+        message: 'How many seconds between automatic invocations?',
+        validate: n => {
+          if (Number.isNaN(parseInt(n, 0))) {
+            return 'Invalid number, requires integer'
+          }
+          if (n < 1) {
+            return 'Number must be greater than 1'
+          }
+          return true
+        },
+        result: choice => ({
+          StartInterval: choice
+        })
+      },
+      Calendar: {
+        type: ''
+      }
+      // 'Watch Path': {}
+    }[triggerMethod]
+  )
+  Object.assign(pList, triggerConfig)
 
-module.exports.main = main
+  // sudo cp ~/Downloads/com.zerowidth.launched.mqtt-benthos-helper.plist /Library/LaunchDaemons
+  // sudo launchctl load -w /Library/LaunchDaemons/com.zerowidth.launched.mqtt-benthos-helper.plist
 
-function getArgs(argv) {
-  const args = require('yargs/yargs')(argv.slice(2))
-    .command('execute [action]', 'execute specified action', yargs => {
-      yargs.positional('action', {
-        describe: 'cli action',
-        default: 'unknown'
-      })
+  // const throttleSecs = await getResult({
+  //   type: 'numeric',
+  //   message: 'Throttle invocations?'
+  // })
+  if (
+    await getResult({
+      type: 'confirm',
+      initial: true,
+      message: 'Keep the process alive?'
     })
-    .command('modify [target]', 'invoke cli against target', yargs => {
-      yargs.positional('target', {
-        describe: 'cli target',
-        default: 'unknown'
+  ) {
+    const alwaysAlive = await getResult({
+      type: 'toggle',
+      message: 'Keep Alive always?',
+      initial: true,
+      enabled: 'Yes',
+      disabled: 'No, selectively'
+    })
+    if (alwaysAlive) {
+      pList.KeepAlive = true
+    } else {
+      const keepAliveOpts = await getResult({
+        type: 'multiselect',
+        message: 'Which factors should determine KeepAlive status?',
+        choices: [
+          'Crashed',
+          'NetworkState',
+          'SuccessfulExit'
+          // { name: 'Depending on the existence of a path', value: 'PathState' },
+        ]
       })
-    }).argv
+      for (let ii = 0; ii < keepAliveOpts.length; ii++) {
+        const element = keepAliveOpts[ii]
+        pList.KeepAlive[element] = await getResult(
+          {
+            Crashed: {
+              type: 'toggle',
+              message: 'How should it respond to Crashed',
+              disabled: 'restart unless it crashes',
+              enabled: 'restart job after it crashes'
+            },
+            NetworkState: {
+              type: 'toggle',
+              message: 'How should it respond to NetworkState',
+              enabled: 'restart when the system has network issues',
+              disabled:
+                'do not restart the job solely because the system has network issues'
+            },
+            SuccessfulExit: {
+              type: 'toggle',
+              message: 'How should it respond to SuccessfulExit',
+              disabled: 'restart until job succeeds',
+              enabled: 'restart until job fails'
+            }
+          }[element]
+        )
+      }
+    }
+    // ...depending on the Existence of a Path: PathState
+    // Use this subkey to keep a job alive as long as a given path exists (true) or does not exist (false).
+    // KeepAlive = { PathState: '/tmp/runJob': true }
+  }
 
-  return Promise.resolve(args)
+  // MISC
+  // AbandonProcessGroup
+  // When launchd wants to terminate a job it sends a SIGTERM signal which will be propagated to all child processes of the job as well. Setting the value of this key to true will stop this propagation, allowing the child processes to survive their parents.
+
+  pList.Label = `local.npm-launchd-wizard.${label}`
+  pList.ProgramArguments = cmd.split(' ')
+  // pList.LaunchOnlyOnce = argz.onlyOnce
+  // pList.RunAtLoad = argz.runAtLoad
+  return pList
 }
 
-module.exports.getArgs = getArgs
+async function addPlist(argz) {
+  const plist = await generatePlist()
+  const plistStr = plistBuilder(plist)
+  if (!argz.daemon) {
+    throw Error('fuck you think you doing')
+  }
+  fs.writeFileSync(`~/Library/LaunchAgents/${plist.label}`, plistStr)
+}
 
-module.exports.run = (...args) => getArgs(args).then(main)
+const main = async () =>
+  require('yargs')
+    .command(
+      'mk',
+      'create a new plist file for launchd',
+      yargs => {
+        yargs.option('daemon', {
+          type: 'boolean',
+          description: 'Create a LaunchDaemon instead of a LaunchAgent'
+        })
+      },
+      addPlist
+    )
+    .command(
+      'list',
+      'list plist files created by this app',
+      yargs => {
+        yargs
+      },
+      () => {
+        fs.readdir(
+          `${process.env.HOME}/Library/LaunchAgents`,
+          (_err, items) => {
+            items.forEach(item => {
+              console.log(item)
+            })
+          }
+        )
+      }
+    )
+    .demand(1, 'Please specify one of the commands!')
+    .help().argv
+
+module.exports.run = main

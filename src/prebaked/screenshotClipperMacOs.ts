b9d94358a7cf3aafc8343b1540a6dfe305667ca3
Jack McCown <jmccown@cbinsights.com>
2020-03-11 22:09:59 -0400

    fix: escape backslashes

--- src/prebaked/screenshotClipperMacOs.ts
+++ src/prebaked/screenshotClipperMacOs.ts
@@ -9,14 +9,14 @@ const script = `
 # as a hidden file, and then renamed to be visible
 /usr/local/bin/fswatch --event Renamed -0 ${process.env.HOME}/Desktop | while read -d "" event; do
   if echo "$event" | grep -q "\/Screen Shot"; then
-    osascript \
-        -e 'set this_file to POSIX file "'"$event"'" as alias' \
-        -e 'tell application "Image Events"' \
-            -e 'launch' \
-            -e 'set this_image to open this_file' \
-            -e 'copy the image file of this_image to imgfile' \
-            -e 'set the clipboard to (read imgfile as PNG)' \
-            -e 'close this_image' \
+    osascript \\
+        -e 'set this_file to POSIX file "'"$event"'" as alias' \\
+        -e 'tell application "Image Events"' \\
+            -e 'launch' \\
+            -e 'set this_image to open this_file' \\
+            -e 'copy the image file of this_image to imgfile' \\
+            -e 'set the clipboard to (read imgfile as PNG)' \\
+            -e 'close this_image' \\
         -e 'end tell'
 
     osascript -e 'display notification "Copied screenshot to clipboard!"'
